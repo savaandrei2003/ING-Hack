5325d9ba738d7188aed769092b6b507850a3f69f
savaandrei2003 <savaandreidragos2@gmail.com>
2024-04-07 01:42:09 +0300

    scapam de font

--- frontend/src/common/components/Page.js
+++ frontend/src/common/components/Page.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './page.css';
 
-import { faHamburger, faHome, faMap, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faHamburger, faHome, faMap, faSearch } from '@fortawesome/free-solid-svg-icons';
+
 import { Route, Link } from 'react-router-dom';
 
 
 export default function Page({ children }) {
 
   const icons = [
-    { icon: faHome, label: 'acasa' },
-    { icon: faMap, label: 'camera' },
-    { icon: faSearch, label: 'Cauta' },
-    { icon: faHamburger, label: 'chat' }
+    { icon: 1, label: 'acasa' },
+    { icon: 2, label: 'camera' },
+    { icon: 3, label: 'Cauta' },
+    { icon: 4, label: 'chat' }
   ];
 
   return (
@@ -33,7 +33,8 @@ export default function Page({ children }) {
           {icons.map((icon, index) => (
             <div className='flex flex-col items-center justify-center' key={index}>
               <Link to={`/${icon.label.toLowerCase()}`}> 
-                <FontAwesomeIcon icon={icon.icon} className='text-orange' />
+                {/* <FontAwesomeIcon icon={icon.icon} className='text-orange' /> */}
+                <p className='text-black text-sm'>{icon.label}</p>
               </Link>
             </div>
           ))}
